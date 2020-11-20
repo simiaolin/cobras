06020751be3f6d4f70facf14f061523dbf152abe
simiaolin <simiao.lin@student.kuleuven.be>
2020-11-20 16:31:19 +0800

    plot 3 types of curve 1. all the curves 2. representative curves of the super instances 3. dba curves of the super instances. 4. dba curve of cluster

--- examples/run_cobras_dtw.py
+++ examples/run_cobras_dtw.py
@@ -3,11 +3,14 @@ import os
 import numpy as np
 from dtaidistance import dtw
 from sklearn import metrics
-
 from cobras_ts.cobras_dtw import COBRAS_DTW
 from cobras_ts.querier.labelquerier import LabelQuerier
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import DBA as dba
 
-ucr_path = '/home/toon/Downloads/UCR_TS_Archive_2015'
+
+ucr_path = '/Users/ary/Desktop/Thesis/UCR_TS_Archive_2015'
 dataset = 'ECG200'
 budget = 100
 alpha = 0.5
@@ -29,3 +32,58 @@ clusterer = COBRAS_DTW(affinities, LabelQuerier(labels), budget)
 clustering, intermediate_clusterings, runtimes, ml, cl = clusterer.cluster()
 
 print(metrics.adjusted_rand_score(clustering.construct_cluster_labeling(),labels))
+
+
+x = np.arange(1,97)
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.set_title('Scatter Plot')
+plt.xlabel('X')
+plt.ylabel('Y')
+
+method = 3
+cluster_idx = 0
+
+#all the curve
+if method == 0:
+    color_arr = cm.rainbow(np.linspace(0, 1, 100))
+    colors = iter(color_arr)
+    for super_instance in clustering.clusters[cluster_idx].size:
+        cur_indices = super_instance.indices
+        cur_color = next(colors)
+        for index in cur_indices:
+            y = series[index]
+            plt.plot(x, y, color=cur_color)
+
+#representative curve
+elif method == 1:
+    color_arr = cm.rainbow(np.linspace(0, 1, len(clustering.clusters[cluster_idx].super_instances)))
+    colors = iter(color_arr)
+    for super_instance in clustering.clusters[cluster_idx].super_instances:
+        cur_color = next(colors)
+        plt.plot(x,series[super_instance.representative_idx], color = cur_color)
+
+#dsa curve
+elif method == 2:
+    color_arr = cm.rainbow(np.linspace(0, 1, len(clustering.clusters[cluster_idx].super_instances)))
+    colors = iter(color_arr)
+
+    for super_instance in clustering.clusters[cluster_idx].super_instances:
+        cur_color = next(colors)
+        cur_indices = super_instance.indices
+        cur_series = list(map(lambda idx: series[idx], cur_indices))
+        series_mean = dba.performDBA(cur_series, 10)
+        plt.plot(x, series_mean, color = cur_color)
+
+else:
+    cur_indices = []
+    for super_instance in clustering.clusters[cluster_idx].super_instances:
+        cur_indices.extend(super_instance.indices)
+    cur_series = list(map(lambda idx: series[idx], cur_indices))
+    series_mean = dba.performDBA(cur_series, 10)
+    plt.plot(x, series_mean, color = 'purple')
+
+
+plt.show()
