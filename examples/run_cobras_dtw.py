9356c1ef0675f58107a5c6c64e6915aedf37d636
simiaolin <simiao.lin@student.kuleuven.be>
2020-12-05 00:39:08 +0800

    [ADD] overall diasplay.
    1. all series + representative ones + dba
    2. DTW horizontal + DTW vertical + vertical

--- examples/run_cobras_dtw.py
+++ examples/run_cobras_dtw.py
@@ -38,35 +38,39 @@ x = np.arange(1,97)
 
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.set_title('Scatter Plot')
-plt.xlabel('X')
-plt.ylabel('Y')
+ax1 = fig.add_subplot(231)
+ax1.set_title('DBA')
+plt.xlabel('idx')
+plt.ylabel('value')
 
 method = 3
 cluster_idx = 0
 
-#all the curve
-if method == 0:
-    color_arr = cm.rainbow(np.linspace(0, 1, 100))
-    colors = iter(color_arr)
-    for super_instance in clustering.clusters[cluster_idx].size:
-        cur_indices = super_instance.indices
-        cur_color = next(colors)
-        for index in cur_indices:
-            y = series[index]
-            plt.plot(x, y, color=cur_color)
+def plotAllCurves(plt):
+    superinstances = clustering.clusters[cluster_idx].super_instances
+    indices_of_current_cluster = []
+    for superinstance in superinstances:
+        indices_of_current_cluster += superinstance.indices
+    plt.set_title('series ' + 'count=' + str(len(indices_of_current_cluster)))
 
-#representative curve
-elif method == 1:
+    color_arr = cm.rainbow(np.linspace(0, 1, len(indices_of_current_cluster)))
+    colors = iter(color_arr)
+
+    for index in indices_of_current_cluster:
+        cur_color = next(colors)
+        y = series[index]
+        plt.plot(x, y, color=cur_color)
+
+def plotRepresentativeCurves(plt):
+    plt.set_title('representative series ' + 'count=' + str(len(clustering.clusters[cluster_idx].super_instances)))
+
     color_arr = cm.rainbow(np.linspace(0, 1, len(clustering.clusters[cluster_idx].super_instances)))
     colors = iter(color_arr)
     for super_instance in clustering.clusters[cluster_idx].super_instances:
         cur_color = next(colors)
-        plt.plot(x,series[super_instance.representative_idx], color = cur_color)
+        plt.plot(x, series[super_instance.representative_idx], color=cur_color)
 
-#dsa curve
-elif method == 2:
+def plotDSACurves():
     color_arr = cm.rainbow(np.linspace(0, 1, len(clustering.clusters[cluster_idx].super_instances)))
     colors = iter(color_arr)
 
@@ -75,15 +79,40 @@ elif method == 2:
         cur_indices = super_instance.indices
         cur_series = list(map(lambda idx: series[idx], cur_indices))
         series_mean = dba.performDBA(cur_series, 10)
-        plt.plot(x, series_mean, color = cur_color)
-#dsa curve of the cluster
-else:
+        plt.plot(x, series_mean, color=cur_color)
+
+def plotDSA_Variance():
     cur_indices = []
     for super_instance in clustering.clusters[cluster_idx].super_instances:
         cur_indices.extend(super_instance.indices)
     cur_series = list(map(lambda idx: series[idx], cur_indices))
-    series_mean, series_variance = dba.performDBA(cur_series, 10)
-    plt.plot(x, series_mean, color = 'purple')
+
+    series_mean, series_dtw_horiz_var, series_dtw_vertic_var, series_vertic_var = dba.performDBA(cur_series, 10)
+
+    ax2 = fig.add_subplot(232)
+    ax3 = fig.add_subplot(233)
+    ax4 = fig.add_subplot(234)
+    ax5 = fig.add_subplot(235)
+    ax6 = fig.add_subplot(236)
+
+
+
+    ax4.set_title('DTW Horizontal Variance')
+    ax5.set_title('DTW Vertical Variance')
+    ax6.set_title('Normal Vertical Variance')
+
+    ax1.plot(x, series_mean, color='purple')
+    plotAllCurves(ax2)
+    plotRepresentativeCurves(ax3)
+    # ax2.plot(x, series_vertic_var, color='grey')
+    # ax3.plot(x, series_dtw_horiz_var, color='green')
+    ax4.plot(x, series_dtw_horiz_var, color='green')
+    ax5.plot(x, series_dtw_vertic_var, color='red')
+    ax6.plot(x, series_vertic_var, color='grey')
+
+plotDSA_Variance()
+
+
 
 
 plt.show()
